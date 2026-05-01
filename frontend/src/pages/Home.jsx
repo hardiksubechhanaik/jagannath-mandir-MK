8a3dffb7820c643dfbb819f209d16dd547366894
hardiksubechhanaik <65248398+hardiksubechhanaik@users.noreply.github.com>
2026-05-01 16:14:47 +0530

    Update Home.jsx
    removed the direct h1 caution tags

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,8 +6,6 @@ import UpcomingEvents from "../components/home/UpcomingEvents"
 const Home = () => {
     return (
         <>
-            <h1>This website is under development please ignore any discrepancy in Information</h1>
-            <h1> ଏହି ୱେବସାଇଟ୍ ନିର୍ମାଣାଧୀନ ଅଛି, ଦୟାକରି ସୂଚନାରେ ଥିବା କୌଣସି ଅସଙ୍ଗତିକୁ ଅଣଦେଖା କରନ୍ତୁ। </h1>
             <Hero />
             <div className="relative pb-20">
                 {/* Doodle background */}

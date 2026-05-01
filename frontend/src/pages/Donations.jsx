9f4930bd615a566a2e00d175ecf4e990fd8601f5
hardiksubechhanaik <65248398+hardiksubechhanaik@users.noreply.github.com>
2026-05-01 16:25:16 +0530

    Update Donations.jsx caution
    caution added

--- frontend/src/pages/Donations.jsx
+++ frontend/src/pages/Donations.jsx
@@ -3,10 +3,12 @@ import DonorDetails from "../components/donationPage/DonorDetailsForm";
 import Hero from "../components/donationPage/Hero"
 import TrustInfo from "../components/donationPage/TrustInfo";
 import Payment from "../components/donationPage/UpiImage";
+import Caution from "../components/caution/caution";
 
 const Donation = () => {
     return (
         <>
+        <Caution/>
         <Hero/>
         <DonationAmounts/>
         <Payment/>
